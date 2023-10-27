fd40785120a7683001511739407f1c71a054b12e
Érica <140272402+ericassilva@users.noreply.github.com>
2023-10-27 19:49:13 -0300

    Add files via upload

---
+++ aula-1.js
@@ -0,0 +1,16 @@
+// Estudando variáveis
+
+ const nome = "Érica";
+ let idade = 35;
+
+ let numerSsoma = 5;
+ let numeroSoma2 = 8;
+
+ let soma = numerosoma + numeroSoma2;
+
+
+ console.log(soma)
+
+ // Concatenando as variáveis
+
+ //console.log ("Olá, me chamo  "+nome+", e tenho "+idade+" anos");
---
+++ aula-2.js
@@ -0,0 +1,30 @@
+//Verificando o tamnanho /comprimento de uma variável
+
+const minhaVariavel = "aprendendoJavascript";
+const tamanhoVariavel = minhaVariavel.length
+//console.log(tamanhoVariavel);
+
+//Substituindo valores de variáveis usando o  .replace()
+
+let mudandoValores = "SubstituindoJS".replace('JS , "Javascript');
+//console.log(mudandoValores);
+
+
+let dividindoString = "dividindoString".split("i");
+//console.log(dividingoString);
+
+//Transforma um texto que está em maiscúlo, para minusculo
+let minuscula = "MINUSCULA".toLowerCase();
+//console.log(minuscula);
+
+//Transforma um texto que está em minusculo, para maiscúlo
+let maiscula = "maiscula".toUpperCase();
+//console.log(maiscula);
+
+//Retorne uma substring/pedaço da minha variável, começando em 0, e indo até no index.
+const frase = "Aprendendo JavaScript".substring(0,11);
+//console.log(frase);
+
+//Retorne uma substring/pedaço da minha variável, começando em 0, e indo até 2 no index.
+const frase2 = "Aprendendo JavaScript".substring(0, 2);
+//console.log(frase);
